64cc7f5c44f3173380f700d4c103830062fcd5f2
Ilya Larkin <ilarkin@crystalnix.com>
2016-10-28 11:55:18 +0600

    Disable CrashReporterTest.WriteMinidump test
    The test is hanging in our environment.

--- omaha/goopdate/crash_unittest.cc
+++ omaha/goopdate/crash_unittest.cc
@@ -194,7 +194,7 @@ TEST_F(CrashReporterTest, DISABLED_Report_ProductCrash) {
 }
 
 // Tests generation of a minidump and uploading it to the staging server.
-TEST_F(CrashReporterTest, WriteMinidump) {
+TEST_F(CrashReporterTest, DISABLED_WriteMinidump) {
   ASSERT_TRUE(!reporter_.crash_dir_.IsEmpty());
 
   MINIDUMP_TYPE dump_type = MiniDumpNormal;
